69cd1c81d3c01db1323596f4f416a68544504c18
Miguel Saldana <mig.saldana@gmail.com>
2016-06-03 00:30:56 -0500

    Added gravatar functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
 
   root               'static_pages#home'
   get 'help'      => 'static_pages#help'
   get 'about'     => 'static_pages#about'
   get 'contact'   => 'static_pages#contact'
   get 'signup'    => 'users#new'
+
+  resources :users
   # You can have the root of your site routed with "root"
 
   # root 'welcome#index'
